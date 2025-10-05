992bf0e24f23fb6fac1f3d25bcc4ccf33d6a5258
Chevin Jeon <109643560+Chevinjeon@users.noreply.github.com>
2025-10-05 15:58:15 -0700

    first commit

---
+++ lab19.cpp
@@ -0,0 +1,15 @@
+// COMSC-210 | Lab 19 | Movie Review System with External File Input
+// Author: Chevin Jeon
+// Date: 2025-10-05
+//
+// Goal:
+//  - Create a Movie class that stores movie title and linked list of reviews
+//  - Read review comments from external file
+//  - Generate random ratings between 1.0-5.0
+//  - Use container of Movie objects in main()
+//  - Demonstrate with at least 4 movies, each with 3+ reviews
+//
+// Build (example):
+//   g++ -std=c++11 -Wall -Wextra -pedantic -O2 lab19.cpp -o lab19
+//
+// ---------------------------------------------------------------------
